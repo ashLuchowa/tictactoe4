0bf464a3eaf1ea58d2adca7919272f7c166ae489
Ash Luchowa <ashwinluchowa@gmail.com>
2025-01-26 10:44:16 +0800

    Create display board UI

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
 //Gameboard object
-const gameBoard = (function() {
-    const firstRow = [1,2,3];
-    const secondRow = [4,5,6];
-    const thirdRow = [7,8,9];
-    return {firstRow, secondRow, thirdRow};
+const gameBoard = (function () {
+    const gameboardContainer = document.querySelector('.gameboard-container');
+
+    const gameBoardArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+    //Board UI
+    function displayBoard() {
+        const squareDiv = document.createElement('div');
+        squareDiv.classList.add('square-container');
+
+        for (let i = 0; i < gameBoardArray.length; i++) {
+            for (let j = 0; j < gameBoardArray[i].length; j++) {
+                const square = document.createElement('div');
+                square.classList.add('square');
+                square.textContent = gameBoardArray[i][j];
+
+                squareDiv.appendChild(square);
+                gameboardContainer.appendChild(squareDiv);
+            }
+        }
+    }
+
+    return { gameboardContainer, gameBoardArray, displayBoard };
 })();
 
-
-for(let i = 0; i < gameBoard.firstRow.length; i++) {
-    gameBoard.firstRow[i] = 'X';
-    console.log(gameBoard.firstRow[i]);    
-}
-
-console.table(gameBoard);
-
-
-if(gameBoard.firstRow[0] && gameBoard.firstRow[1] && gameBoard.firstRow[2] === 'X') {
-    console.log('player won the game');
-} else {
-    console.log('player lost the game');
-}
+gameBoard.displayBoard();

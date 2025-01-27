06e19abb5e1e2f39c30a9062e7f330d21f3c4328
Ash Luchowa <ashwinluchowa@gmail.com>
2025-01-27 14:12:42 +0800

    Add reset + winUI function

--- js/main.js
+++ js/main.js
@@ -46,20 +46,26 @@ const gameBoard = (function () {
                 gameBoardArray[rowIndex][columnIndex] = playerTurn;
                 console.log({ playerTurn });
 
-                // Change player's turn
-                changePlayerTurn();
                 console.table(gameBoardArray);
             } else {
                 return targetItem.textContent;
             }
+
+            // Display win UI
+            gameLogic();
+
+            // Change player's turn
+            changePlayerTurn();
         });
     }
 
     // Players Turn
     function changePlayerTurn() {
         if (playerTurn === playerX) {
+            displayPlayer = playerX;
             playerTurn = playerY;
         } else {
+            displayPlayer = playerY;
             playerTurn = playerX;
         }
     }
@@ -68,32 +74,51 @@ const gameBoard = (function () {
     function gameLogic() {
 
         //Horizontal
-        if (gameBoardArray[0][0] === 'X' && gameBoardArray[0][1] === 'X' && gameBoardArray[0][2] === 'X' ||
-            gameBoardArray[1][0] === 'X' && gameBoardArray[1][1] === 'X' && gameBoardArray[1][2] === 'X' ||
-            gameBoardArray[2][0] === 'X' && gameBoardArray[2][1] === 'X' && gameBoardArray[2][2] === 'X' ||
+        if (gameBoardArray[0][0] === playerTurn && gameBoardArray[0][1] === playerTurn && gameBoardArray[0][2] === playerTurn ||
+            gameBoardArray[1][0] === playerTurn && gameBoardArray[1][1] === playerTurn && gameBoardArray[1][2] === playerTurn ||
+            gameBoardArray[2][0] === playerTurn && gameBoardArray[2][1] === playerTurn && gameBoardArray[2][2] === playerTurn ||
 
             // Vertical
-            gameBoardArray[0][0] === 'X' && gameBoardArray[1][0] === 'X' && gameBoardArray[2][0] === 'X' ||
-            gameBoardArray[0][1] === 'X' && gameBoardArray[1][1] === 'X' && gameBoardArray[2][1] === 'X' ||
-            gameBoardArray[0][2] === 'X' && gameBoardArray[1][2] === 'X' && gameBoardArray[2][2] === 'X' ||
+            gameBoardArray[0][0] === playerTurn && gameBoardArray[1][0] === playerTurn && gameBoardArray[2][0] === playerTurn ||
+            gameBoardArray[0][1] === playerTurn && gameBoardArray[1][1] === playerTurn && gameBoardArray[2][1] === playerTurn ||
+            gameBoardArray[0][2] === playerTurn && gameBoardArray[1][2] === playerTurn && gameBoardArray[2][2] === playerTurn ||
 
             // Diagonal
-            gameBoardArray[0][0] === 'X' && gameBoardArray[1][1] === 'X' && gameBoardArray[2][2] === 'X' ||
-            gameBoardArray[0][2] === 'X' && gameBoardArray[1][2] === 'X' && gameBoardArray[2][0] === 'X'
+            gameBoardArray[0][0] === playerTurn && gameBoardArray[1][1] === playerTurn && gameBoardArray[2][2] === playerTurn ||
+            gameBoardArray[0][2] === playerTurn && gameBoardArray[1][2] === playerTurn && gameBoardArray[2][0] === playerTurn
         ) {
-            console.log('Player won!');
-        } else {
-            console.log('player lost!');
+            setTimeout(winUI, 250);
         }
     }
 
-    // Click on board
-    // function boardInput() {
-    //     console.log(square);
-    // }
+    // Win Function
+    function winUI() {
+        let winResult = confirm(`${displayPlayer} has won! Continue?`);
+        if(winResult === true) {
+            resetBoard();
+        } else {
+            alert('You chose not to continue');
+        }
+    }
+
+    // Reset Function
+    function resetBoard() {
+        for(let i = 0; i < gameBoardArray.length; i++) {
+            for (let j = 0; j < gameBoardArray[i].length; j++) {
+                // Reset Array
+                gameBoardArray[i][j] = '';
+                console.table(gameBoardArray);
+
+                //Reset UI
+                const squares = document.querySelectorAll('.square');
+                squares.forEach(square => {
+                    square.textContent = '';
+                });
+            }
+        }
+    }
 
     displayBoard();
-    gameLogic();
     createPlayer();
 
     // return { gameboardContainer, gameBoardArray, displayBoard, gameLogic };

108a4ca0b05d8c4686b91e18f63b7b6acfce7aa3
Ash Luchowa <ashwinluchowa@gmail.com>
2025-01-30 03:15:13 +0800

    Add score + round + player handle

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@
 const gameBoard = (function () {
     const gameboardContainer = document.querySelector('.gameboard-container');
     const gameBoardArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    let gameRound = 1;
 
     // Create Players
     function createPlayer(userMarker) {
         let userScore = 0;
+        let userWin = false;
         const getScore = () => userScore;
         const giveScore = () => userScore++;
-        return {userMarker, giveScore, getScore};
+        return {userScore, userMarker, giveScore, getScore, userWin};
     }
 
     // Players
@@ -35,6 +37,60 @@ const gameBoard = (function () {
         }
     }
 
+    // Display the scores, player names and logo
+    function scoreInterface() {
+        // Game logo
+        const gameLogo = document.createElement('div');
+        gameLogo.classList.add('game-logo');
+        gameboardContainer.appendChild(gameLogo);
+        const gameLogoText = document.createElement('h1');
+        gameLogo.appendChild(gameLogoText);
+        gameLogoText.textContent = 'TicTacToe';
+
+        // Player Names
+        const playerXContainer = document.createElement('div');
+        const playerOContainer = document.createElement('div');
+        playerXContainer.classList.add('player-x');
+        playerOContainer.classList.add('player-o');
+        const playerNameX = document.createElement('p');
+        const playerNameO = document.createElement('p');
+        playerXContainer.appendChild(playerNameX);
+        playerOContainer.appendChild(playerNameO);
+        gameboardContainer.appendChild(playerXContainer);
+        gameboardContainer.appendChild(playerOContainer);
+        playerNameX.textContent = `Player ${playerX.userMarker}:`;
+        playerNameO.textContent = `Player ${playerO.userMarker}:`;
+
+        // Game round
+        const gameRoundContainer = document.createElement('div');
+        gameboardContainer.appendChild(gameRoundContainer);
+        gameRoundContainer.classList.add('game-round');
+        let gameRoundText = document.createElement('p');
+        gameRoundContainer.appendChild(gameRoundText);
+        gameRoundText.textContent = `Round ${gameRound}`;
+
+        return { playerXContainer, playerOContainer };
+    }
+
+    // Store returned elements
+    const scoreElements = scoreInterface();
+
+    // Score UI
+    function playerScoreUI() {
+        let playerScoreX = document.createElement('p');
+        let playerScoreO = document.createElement('p');
+
+        scoreElements.playerXContainer.appendChild(playerScoreX);
+        scoreElements.playerOContainer.appendChild(playerScoreO);
+
+        // playerScoreX.textContent = playerX.getScore();
+        // playerScoreO.textContent = playerO.getScore();
+
+        return { playerScoreX, playerScoreO };
+    }
+
+    const playerScoreElements = playerScoreUI();
+
     // Player Input Event
     function playerInput(targetItem, rowIndex, columnIndex) {
         targetItem.addEventListener('click', () => {
@@ -54,19 +110,15 @@ const gameBoard = (function () {
 
             // Change player's turn
             changePlayerTurn();
-
-            console.table(gameBoardArray);
         });
     }
 
     // Players Turn
     function changePlayerTurn() {
         if (playerTurn === playerX) {
-            wonPlayer = playerX;
             displayPlayer = playerX.userMarker;
             playerTurn = playerO;
         } else {
-            wonPlayer = playerO;
             displayPlayer = playerO.userMarker;
             playerTurn = playerX;
         }
@@ -97,14 +149,18 @@ const gameBoard = (function () {
     function winUI() {
         let winResult = confirm(`Player ${displayPlayer} has won! Continue?`);
         if(winResult === true) {
-            wonPlayer.giveScore();
-            resetBoard();
-            console.log(playerX.getScore());
-            console.log(playerO.getScore());
+            if(displayPlayer === 'X') {
+                playerX.giveScore();
+                playerScoreElements.playerScoreX.textContent = playerX.getScore();
+                resetBoard();
+            } else {
+                playerO.giveScore();
+                playerScoreElements.playerScoreO.textContent = playerO.getScore();
+                resetBoard();
+            }
         } else {
             resetBoard();
         }
-        console.table(gameBoardArray);
     }
 
     // Reset Function
@@ -121,10 +177,13 @@ const gameBoard = (function () {
                 });
             }
         }
+        playerX.userWin = false;
+        playerO.userWin = false;
     }
 
     displayBoard();
     createPlayer();
+    playerScoreUI();
 
-    return { gameboardContainer, gameBoardArray, displayBoard, gameLogic, playerX, playerO };
+    return { gameboardContainer, gameBoardArray, displayBoard, gameLogic, playerX, playerO, gameRound };
 })();
